db20a5daab7c704fc73ebe3f87fdee5a0b4c5558
j13tw <j13tw@yahoo.com.tw>
2018-12-06 14:44:26 +0800

    fix socket port

--- socket_2_publish.py
+++ socket_2_publish.py
@@ -7,13 +7,13 @@ import datetime
 import paho.mqtt.client as mqtt
  
 socket_host = "www.google.com"
-socket_port = 8
+socket_port = 9808
 
 mqtt_transport = 0
 mqtt_ip = ''
 mqtt_topic = ''
 mqtt_qos = 0
-
+#publisher 9808 sub 9807
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((socket_host, socket_port))
 client.settimeout(None)
@@ -40,17 +40,31 @@ except:
     mqtt_transport = 0
 
 while(mqtt_transport == 1):
+    data_check = 0
     print("-----------------------------------------------------------")
     response = client.recv(4096).decode('utf-8')
     print("socket data : ", response)
+    print("-----------------------------------------------------------")
     try:
         socket_rec = json.loads(response)
         data = socket_rec['recv']
-        mqtt_conn.connect(mqtt_ip, 1883)
-        mqtt_conn.publish(mqtt_topic, response, mqtt_qos)
-        now = datetime.datetime.now()
-        print('MQTT To Server OK ! -->' , now)
+        print("json data = ", data)
+        data_check = 1
     except:
-        print('MQTT To Server Error ! -->' , now)
+        lient.send('{"mqtt_send":"data_error"}').encode()
+        print("json data error")
+
+    if(data_check == 1):
+        try:
+            mqtt_conn.connect(mqtt_ip, 1883)
+            mqtt_conn.publish(mqtt_topic, response, mqtt_qos)
+            now = datetime.datetime.now()
+            print('MQTT To Server OK ! -->' , now)
+            client.send('{"mqtt_send":"ok"}').encode()
+        except:
+            print('MQTT To Server Error ! -->' , now)
+            client.send('{"mqtt_send":"broker_error"}').encode()
+    else:
+        pass
     print("-----------------------------------------------------------")
     time.sleep(3)
