651e30520db7a02c00352dc8f66cf7512eb8619c
j13tw <j13tw@yahoo.com.tw>
2018-12-11 09:18:02 +0800

    fix mqtt 2 socket transport method

--- subscribe_2_socket.py
+++ subscribe_2_socket.py
@@ -7,16 +7,40 @@ import datetime
 import paho.mqtt.client as mqtt
  
 socket_host = "www.google.com"
-socket_port = 9808
+socket_port = 9807
 
 mqtt_transport = 0
 mqtt_ip = ''
 mqtt_topic = ''
 mqtt_qos = 0
+
 #publisher 9808 sub 9807
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((socket_host, socket_port))
 client.settimeout(None)
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    mqtt_sub.subscribe(mqtt_topic, mqtt_qos)
+
+def on_message(client, userdata, message):
+    now = datetime.datetime.now()
+    year = str(now.year)
+    month = str(now.month)
+    if (int(month) < 10): month = "0" + str(month)
+    day = str(now.day)
+    if (int(day) < 10): day = "0" + str(day)
+    hour = str(now.hour)
+    if (int(hour) < 10): hour = "0" + str(hour)
+    minute = str(now.minute)
+    if (int(minute) < 10): minute = "0" + str(minute)
+    second = str(now.second)
+    if (int(second) < 10): second = "0" + str(second)
+    micro_second = str(int(datetime.datetime.now().microsecond/100))
+    print('------------------------------------------------------')
+    print("message received -->" ,message.payload.decode('utf-8'))
+    print("message topic =",message.topic)
+    client.send(message.payload.decode('utf-8')).encode()
 
 while (mqtt_transport == 0):
     client.send(("mqtt").encode())
@@ -32,14 +56,27 @@ while (mqtt_transport == 0):
         mqtt_transport = 0
 
 try:
-    mqtt_pub = mqtt.Client()
-    mqtt_pub.connect(mqtt_ip, 1883)
+    mqtt_conn = mqtt.Client()
+    mqtt_conn.connect(mqtt_ip, 1883)
     client.send('{"status":"create"}').encode()
 except:
     client.send('{"status":"error"}')
     mqtt_transport = 0
 
 while(mqtt_transport == 1):
+    # MQTT connection
+    mqtt_sub = mqtt.Client()
+    mqtt_sub.on_message = on_message
+    mqtt_sub.on_connect = on_connect
+    try:
+        mqtt_sub.connect(mqtt_ip, 1883)
+        client.send('{"mqtt_recv":"ok"}').encode()
+        mqtt_sub.loop_start()
+        time.sleep(1)
+        mqtt_sub.loop_stop()
+    except:
+        client.send('{"mqtt_recv":"broker_error"}').encode()
+'''
     data_check = 0
     print("-----------------------------------------------------------")
     response = client.recv(4096).decode('utf-8')
@@ -47,7 +84,7 @@ while(mqtt_transport == 1):
     print("-----------------------------------------------------------")
     try:
         socket_rec = json.loads(response)
-        data = socket_rec['send']
+        data = socket_rec['recv']
         print("json data = ", data)
         data_check = 1
     except:
@@ -56,8 +93,8 @@ while(mqtt_transport == 1):
 
     if(data_check == 1):
         try:
-            mqtt_pub.connect(mqtt_ip, 1883)
-            mqtt_pub.publish(mqtt_topic, response, mqtt_qos)
+            mqtt_conn.connect(mqtt_ip, 1883)
+            mqtt_conn.publish(mqtt_topic, response, mqtt_qos)
             now = datetime.datetime.now()
             print('MQTT To Server OK ! -->' , now)
             client.send('{"mqtt_send":"ok"}').encode()
@@ -68,3 +105,4 @@ while(mqtt_transport == 1):
         pass
     print("-----------------------------------------------------------")
     time.sleep(3)
+'''
